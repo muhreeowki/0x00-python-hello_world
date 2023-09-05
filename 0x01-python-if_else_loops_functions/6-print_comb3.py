8e86ac3d215a7e19ca5969185025d5721a3f8d27
Muriuki <muriuki@muchiri.com>
2023-09-05 15:28:14 +0000

    Slight Adjustments

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(9):
     for j in range(i + 1, 10):
         print("{:d}{:d}".format(i, j),
-            end="\n" if (i == 8 and j == 9) else ", ")
+              end="\n" if (i == 8 and j == 9) else ", ")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str.lower():
-        print("{}".format(chr((ord(c) % 97) + 65)\
-		if c >= 'a' and c <= 'z' else c), end="")
+        print("{}".format(chr((ord(c) % 97) + 65)
+              if c >= 'a' and c <= 'z' else c), end="")
     print("")
