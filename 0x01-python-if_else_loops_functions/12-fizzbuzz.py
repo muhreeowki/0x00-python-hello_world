e3529080cc9b2d9a099de2db47468e51e8e11522
Muriuki <muriuki@muchiri.com>
2023-09-05 08:21:29 +0000

    Added tasks 10, 11, and 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+	for i in range(1, 101):
+		if i % 5 == 0 or i % 3 == 0:
+			if (i % 5 == 0 and i % 3 == 0):
+				print("FizzBuzz", end=" ")
+			else:
+			 	print("Fizz" if i % 3 == 0 else "Buzz", end=" ")
+		else:
+			print(i, end=" ")
+		
