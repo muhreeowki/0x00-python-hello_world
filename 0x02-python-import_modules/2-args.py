3e913992290b279232d7905ee7395da879a51546
Muriuki <muriuki@muchiri.com>
2023-09-07 10:47:51 +0000

    Fixing errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc = len(sys.argv) - 1
-    print("{} arguments".format(argc), end=":\n" if argc > 0 else ".\n")
-    for i in range(1, argc):
-        print("{}: {}".format(i, sys.argv[i]))
+    argc = len(sys.argv)
+    print("{} arguments".format(argc if argc > 1 else 0),
+          end=":\n" if argc > 1 else ".\n")
+    if (argc >= 2):
+        for i in range(1, argc):
+            print("{}: {}".format(i, sys.argv[i]))
