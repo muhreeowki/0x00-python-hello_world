984a70bcb9d55bc4029bc3e793ae668d43510261
Muriuki <muriuki@muchiri.com>
2023-09-07 11:51:12 +0000

    Fixing pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
-    print("{} argument{}".format(argc if argc > 0 else 0, "" if argc == 1 else "s"),
+    s = "" if argc == 1 else "s"
+    print("{} argument{}".format(argc if argc > 0 else 0, s),
           end=":\n" if argc > 0 else ".\n")
     for i in range(1, argc + 1):
         print("{}: {}".format(i, sys.argv[i]))
