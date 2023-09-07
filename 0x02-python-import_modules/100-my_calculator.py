34c79202d8f13e93feaede8fb4ad2433906ca4ed
Muriuki <muriuki@muchiri.com>
2023-09-07 09:31:26 +0000

    Added task 8

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ import sys
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
-	
+
 a = int(sys.argv[1])
 operator = sys.argv[2]
 b = int(sys.argv[3])
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 import subprocess
 subprocess.call('echo "#pythoniscool"', shell=True)
-#__import__('os').write(1, b'#pythoniscool\n')
