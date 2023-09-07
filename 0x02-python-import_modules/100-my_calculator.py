ab377efe38abe8aa53da06d0de4301d7763d374b
Muriuki <muriuki@muchiri.com>
2023-09-07 11:18:08 +0000

    Fixing compilation errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,4 +21,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-	exit (1)
+        sys.exit(1)
