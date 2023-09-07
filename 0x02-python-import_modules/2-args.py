f2f3b5ad3c4c17b51aab2389358c560ce8601dd5
Muriuki <muriuki@muchiri.com>
2023-09-07 11:50:09 +0000

    Fixing task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
-    print("{} arguments".format(argc if argc > 0 else 0),
+    print("{} argument{}".format(argc if argc > 0 else 0, "" if argc == 1 else "s"),
           end=":\n" if argc > 0 else ".\n")
     for i in range(1, argc + 1):
         print("{}: {}".format(i, sys.argv[i]))
