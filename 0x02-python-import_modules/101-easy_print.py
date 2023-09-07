f3df3b1d44d43b438559f9e1471f34f3437d0502
Muriuki <muriuki@muchiri.com>
2023-09-07 10:35:25 +0000

    Fixing errors

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import subprocess
-    subprocess.call('echo "#pythoniscool"', shell=True)
+import subprocess
+subprocess.call('echo "#pythoniscool"', shell=True)
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
     print("{} arguments".format(argc), end=":\n" if argc > 0 else ".\n")
-    for i in range(1, argc + 1):
+    for i in range(1, argc):
         print("{}: {}".format(i, sys.argv[i]))
