3b515ffc4f3c7d16426fa95642f962a0cd3a300c
Muriuki <muriuki@muchiri.com>
2023-09-07 07:14:02 +0000

    Added task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+argc = len(sys.argv) - 1
+print("{} arguments".format(argc), end=":\n" if argc > 0 else ".\n")
+for i in range(1, argc + 1):
+	print("{}: {}".format(i, sys.argv[i]))
