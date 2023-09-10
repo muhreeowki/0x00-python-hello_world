ad754a4987f99d2ea4c5a498a17818350a4e411d
Muriuki <muriuki@muchiri.com>
2023-09-10 13:25:37 +0000

    Added task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,5 @@
-#!/usr/bin/python3
-
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            print("{}".format(row[i]), end=" " if i < len(row) - 1 else "")
+        print()
