0559240daacb667b60894d41f13d69848dcd4c0a
Muriuki <muriuki@muchiri.com>
2023-09-12 18:22:59 +0000

    Added task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        new_list = []
+        for i in range(len(my_list)):
+            new_list.append(True if my_list[i] % 2 == 0 else False)
+        return new_list
+    return []
