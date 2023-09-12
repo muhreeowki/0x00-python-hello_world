c2b5a348389e2711f72cea1d5694752925f703f9
Muriuki <muriuki@muchiri.com>
2023-09-12 18:30:55 +0000

    Added task 11

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = 0 if sentence == None else len(sentence)
-    character = None if length == 0 else sentence[0]
+    length = 0 if sentence is None else len(sentence)
+    character = None if length is 0 else sentence[0]
     return (length, character)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        highest = my_list[0];
+        highest = my_list[0]
         for num in my_list:
             highest = num if num > highest else highest
         return highest
