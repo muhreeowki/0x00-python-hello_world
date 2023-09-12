5f180902172ccd3357bce0a0b7e1903e9e8d04ee
Muriuki <muriuki@muchiri.com>
2023-09-12 18:11:57 +0000

    Added task 7 and 8

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+multiple_returns = __import__('8-multiple_returns').multiple_returns
 
-print_reversed_list_integer()
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
