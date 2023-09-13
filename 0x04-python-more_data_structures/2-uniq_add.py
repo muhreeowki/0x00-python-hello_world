67b31b2bde387e352fcb0933f4d8bdb9ad30d634
Muriuki <muriuki@muchiri.com>
2023-09-13 10:49:19 +0000

    Added task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    total = 0
+
+    if (my_list):
+        uniq = set(my_list)
+        for num in uniq:
+            total += num
+    
+    return total
