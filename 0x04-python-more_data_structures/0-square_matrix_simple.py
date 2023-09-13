e7cc9083d524d28d2dd638aa6811e50470557e50
Muriuki <muriuki@muchiri.com>
2023-09-13 10:39:13 +0000

    Added task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+    new_matrix = []
+    if matrix:
+        for i in range(len(matrix)):
+            row = []
+            for j in range(len(matrix[i])):
+                row.append(matrix[i][j] * matrix[i][j])
+            new_matrix.append(row)
+    return new_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+Learning more about data structures in python
