69f07ae745f61169cc845bbaa90a884ac6c8f157
Muriuki <muriuki@muchiri.com>
2023-09-23 10:18:21 +0000

    Added task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,84 @@
 #include "lists.h"
 
+
 int is_palindrome(listint_t **head)
 {
+	/* if palindrome return 1 */
+	/* if not palindrome return 0 */
+	/* loop through the list forward and backwards */
+	/* if the characters dont match, it is not a palindrome*/
 
+
+	listint_t *stack = malloc(sizeof(listint_t));
+	listint_t *node = NULL;
+	int node2;
+
+	if (head == NULL || *head == NULL)
+		return (1);
+
+	node = *head;
+
+	/* loop through the linked list */
+	while (node)
+	{
+		/* add the node to the stack */
+		add_nodeint_end(&stack, node->n);
+		node = node->next;
+	}
+
+	node = *head;
+
+	while (node)
+	{
+		node2 = pop(&stack);
+		if (node2 != node->n)
+			return(0);
+		node = node->next;
+	}
+	return (1);
+}
+
+
+/**
+ * delete_dnodeint_at_index - deletes a node at an index
+ *
+ * @head: head of a dlistint_t list.
+ * @index: indext of the node to delete.
+ *
+ * Return: sum of elements
+ */
+int pop(listint_t **head)
+{
+	listint_t *node = NULL, *prev = NULL;
+	int data;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if ((*head)->next == NULL)
+	{
+		data = (*head)->n;
+		free(*head);
+		*head = NULL;
+	}
+
+	else
+	{
+		prev = *head;
+		node = (*head)->next;
+		while(node)
+		{
+			if (node->next == NULL)
+			{
+				data = node->n;
+				free(node);
+				prev->next = NULL;
+				break;
+			}
+			prev = node;
+			node = node->next;
+		}
+	}
+
+	return (data);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -9,12 +11,14 @@
  * Description: singly linked list node structure
  * for project
  */
+
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
 
+int pop(listint_t **head);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
