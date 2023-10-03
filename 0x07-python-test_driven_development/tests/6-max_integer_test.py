1936d38e6e837bd9cffd67bf6f292de0d6ee6399
Muriuki <muriuki@muchiri.com>
2023-10-03 16:59:57 +0000

    Added task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,12 +1,20 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 import unittest
-
-max_integer = __import__("../6-max_integer").max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-
-    test_normal():
-    test_list_with_string():
-    test_list_with_negatives():
-    test_list_with_floats():
-    test_list_with_one_item():
-    test_empty_list():
+    def test_normal(self):
+        self.assertIs(max_integer([1, 2, 3, 4]), 4)
+        self.assertIs(max_integer([1, 10, 3, 4]), 10)
+    def test_empty_list(self):
+        self.assertIs(max_integer([]), None)
+    def test_list_with_string(self):
+        self.assertRaises(TypeError, max_integer(['1', '2', '3', '4']))
+    def test_list_with_negatives(self):
+        self.assertIs(max_integer([1, -210, -30, -94]), 1)
+    def test_list_with_floats(self):
+        self.assertIs(max_integer([1.321, 7.8934, 910.324, 28.12839, 3.23012]), 910.324)
+    def test_list_with_one_item(self):
+        self.assertIs(max_integer([69]), 69)
