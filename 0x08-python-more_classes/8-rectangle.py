c2b446bc3743215eec5468488a914fb3573645ab
Muriuki <muriuki@muchiri.com>
2023-10-03 13:00:14 +0000

    Fixing task 7 and 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -23,6 +23,7 @@ class Rectangle():
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
