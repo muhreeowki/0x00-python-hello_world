3e441127616ce39f2f406b65f9d8857b14186fd2
Muriuki <muriuki@muchiri.com>
2023-10-03 12:55:47 +0000

    Added height and width checks to init method for tasks 2 - 9

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -11,8 +11,18 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """This is a constructor method"""
-        self.__height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
 
     def __str__(self):
         """
