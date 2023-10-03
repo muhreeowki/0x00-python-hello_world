d161301db27eb24e26d1d827fc7d77a94af2fb2b
Muriuki <muriuki@muchiri.com>
2023-10-03 08:03:13 +0000

    Added type check into init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,7 +11,16 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """This is a constructor method"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__height = height
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
 
     @property
