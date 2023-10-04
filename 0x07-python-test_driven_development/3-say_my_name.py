e4ffe03053b42bbc67d37b825f9d6f978a965e5a
Muriuki <muriuki@muchiri.com>
2023-10-04 09:13:30 +0000

    Added task 4 and 5

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,7 @@
 """
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name="", last_name=""):
     """
     Function that prints My name is <first name> <last name>
 
@@ -19,8 +19,10 @@ def say_my_name(first_name, last_name=""):
         TypeError: If first_name or last_name is not a string
 
     """
-    if not isinstance(first_name, str):
+    if not isinstance(first_name, str) or first_name == "":
         raise TypeError("first_name must be a string")
+
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     print("My name is {:s} {:s}".format(first_name, last_name))
