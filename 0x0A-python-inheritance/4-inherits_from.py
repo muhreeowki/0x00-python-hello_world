7e72f936d09e4911aea9335aec04dbbb12d89581
Muriuki <muriuki@muchiri.com>
2023-10-09 07:32:11 +0000

    Added task 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,9 +5,9 @@ Module that contains inherits_from function
 
 def inherits_from(obj, a_class):
     """
-    Function that returns True if the object is an instance of,
-    or if the object is an instance of a class that inherited from,
-    the specified class ; otherwise False
+    Function that returns True if the object is an
+    instance of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False
     """
     return True if \
         type(obj) in a_class.__subclasses__() or a_class.__bases__ else False
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,5 @@
+"""Module that contains a Class called BaseGeometry"""
+
+
+class BaseGeometry():
+    """This is an empty class"""
