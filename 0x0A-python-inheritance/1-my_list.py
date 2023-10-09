05a56341b936c158629bb78b04a8272a3b79e382
Muriuki <muriuki@muchiri.com>
2023-10-09 07:01:04 +0000

    Added task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+"""Module that contains a Class called MyList inherited from the list class"""
+
+class MyList(list):
+    """This is a child class of the list class"""
+    def print_sorted(self):
+        """Method that prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
