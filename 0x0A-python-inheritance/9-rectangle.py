c6fd351cc275433857dad9d32bd44271af8422a4
Muriuki <muriuki@muchiri.com>
2023-10-09 08:27:37 +0000

    Added task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+"""Module that contains a Class called Rectangle"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    Class that represents a rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        return self.__width * self.__height
