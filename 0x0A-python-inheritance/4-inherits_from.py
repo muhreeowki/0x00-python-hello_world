f1fa4064148ebdff64d382f832f7047399898e9a
Muriuki <muriuki@muchiri.com>
2023-10-09 11:09:54 +0000

    Fixing task 7

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,5 @@ def inherits_from(obj, a_class):
     instance of a class that inherited (directly or indirectly)
     the specified class ; otherwise False
     """
-    return issubclass(type(obj), a_class) if type(obj) is not a_class else False
+    return issubclass(type(obj), a_class) if type(obj) is not a_class \
+        else False
