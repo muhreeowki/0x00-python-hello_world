b3976d1a66fd5a5db85676c2347cfe1b3abab3f9
Muriuki <muriuki@muchiri.com>
2023-10-09 06:39:14 +0000

    Added task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module containing a lookup funtion"""
+
+def lookup(obj):
+    """Function that returns the list of available attributes and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+Learning about Inheritance in python
