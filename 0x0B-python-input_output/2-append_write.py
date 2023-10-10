bc0e7d5919684674ed6523f9aa13663720811464
Muriuki <muriuki@muchiri.com>
2023-10-10 08:28:04 +0000

    Added task 3

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module containing append_write funtion"""
 
+
 def append_write(filename="", text=""):
     """Function that appends a string at the end of a text file
     and returns the number of characters added"""
