8278ea5e8081c8869e39208dfcb7b173286e9f2f
Muriuki <muriuki@muchiri.com>
2023-10-10 08:48:07 +0000

    Added task 6

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,7 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """unction that writes an Object to a text file,
+    """Function that writes an Object to a text file,
     using a JSON representation"""
     with open(filename, 'w') as f:
         f.write(json.dumps(my_obj))
