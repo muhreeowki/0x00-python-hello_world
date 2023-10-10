bd19a75586ec84e8b29a0e176d78d168e5770c21
Muriuki <muriuki@muchiri.com>
2023-10-10 10:05:52 +0000

    Fixing task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,15 +11,15 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 items_list = []
 
-if len(sys.argv) >= 2:
-    for i in range(1, len(sys.argv)):
-        items_list.append(sys.argv[i])
-
 try:
-    old = load_from_json_file("add_item.json")
-    for item in old:
+    old_list = load_from_json_file("add_item.json")
+    for item in old_list:
         items_list.append(item)
 except FileNotFoundError:
     pass
+
+if len(sys.argv) >= 2:
+    for i in range(1, len(sys.argv)):
+        items_list.append(sys.argv[i])
 
 save_to_json_file(items_list, "add_item.json")
