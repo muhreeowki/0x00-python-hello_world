643d1203a23f73110481f6e79875480b09d521e1
Muriuki <muriuki@muchiri.com>
2023-10-10 10:57:05 +0000

    Fixing task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,4 +9,4 @@ def class_to_json(obj):
     simple data structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
     """
-    return(obj.__dict__)
+    return obj.__dict__
