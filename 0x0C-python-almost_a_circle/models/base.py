917fce37797979effa217a500e0ef77f6280a353
Muriuki <muriuki@muchiri.com>
2023-10-18 13:36:58 +0300

    Making sure all tests work

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,15 +22,6 @@ class Base:
             return json.dumps([])
         return json.dumps(list_dictionaries)
 
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Saves a json representation of a list of objects to a file"""
-        obj_strings = []
-        for obj in list_objs:
-            obj_strings.append(obj.to_dictionary())
-        with open("{}.json".format(cls.__name__), "w") as f:
-            f.write(cls.to_json_string(obj_strings))
-        
     @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string"""
@@ -38,7 +29,7 @@ class Base:
         if json_string is not None and json_string != "":
             json_list = json.loads(json_string)
         return json_list
-
+        
     @classmethod
     def create(cls, **dictionary):
         """
@@ -48,6 +39,16 @@ class Base:
         instance = cls(10, 10)
         instance.update(**dictionary)
         return instance
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Saves a json representation of a list of objects to a file"""
+        obj_strings = []
+        if list_objs:
+            for obj in list_objs:
+                obj_strings.append(obj.to_dictionary())
+        with open("{}.json".format(cls.__name__), "w") as f:
+            f.write(cls.to_json_string(obj_strings))
 
     @classmethod
     def load_from_file(cls):
@@ -72,10 +73,27 @@ class Base:
         Returns a list of instances that were stored
         in a json file
         """
-        instance_list = []
         try:
+            objs_list = []
             with open("{}.csv".format(cls.__name__), 'r') as f:
                 reader = csv.DictReader(f)
+                for obj_dict in reader:
+                    for key, value in obj_dict.items():
+                         obj_dict[key] = int(value)
+                    objs_list.append(cls.create(**obj_dict))
+                return objs_list
         except FileNotFoundError:
             pass
-        return instance_list
+        return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Saves a json representation of a list of objects to a file"""
+        with open("{}.csv".format(cls.__name__), "w") as f:
+            if list_objs and len(list_objs) != 0:
+                fields = list_objs[0].to_dictionary().keys()
+                writer = csv.DictWriter(f, fieldnames=fields)
+                writer.writeheader()
+                writer.writerows([obj.to_dictionary() for obj in list_objs])
+            else:
+                f.write("[]")
