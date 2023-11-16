7687a76cdc12e531bc24468bdda48c11dec345fc
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-16 13:16:29 +0300

    Added task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Script that creates the MySQL server user user_0d_1.
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'NewPassword';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
