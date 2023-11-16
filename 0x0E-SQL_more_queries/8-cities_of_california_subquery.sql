c2105f860a2020fb3518e17c55f0195f0df9973d
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-16 14:40:44 +0300

    Added task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,6 @@
--- Script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-  );
+-- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT id, name FROM cities 
+WHERE stated_id IN (
+  SELECT id FROM states 
+  WHERE name = 'California'
+) ORDER BY id ASC;
