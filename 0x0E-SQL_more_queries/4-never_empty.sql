087de2de31557e817de7abda7ca8a91ffe8e0778
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-16 13:45:40 +0300

    Added task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- Script that creates the table id_not_null on your MySQL server.
 DROP TABLE IF EXISTS force_name;
-CREATE TABLE force_name (id INT SET DEFAULT 1, name VARCHAR(256));
+CREATE TABLE force_name (id INT DEFAULT 1, name VARCHAR(256));
