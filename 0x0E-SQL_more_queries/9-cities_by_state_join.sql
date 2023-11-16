14cc43e2495440f37f2ff8bb6296575b6ee0ce65
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-16 15:01:10 +0300

    Added task 8

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name
+FROM cities FULL OUTER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id;
