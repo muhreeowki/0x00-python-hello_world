e2a20c098d3d22cfb86713cf512d8e11a1156a25
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-28 10:46:27 +0300

    Added tasks 10 - 20

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
--- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT s.title, g.genre_id
-FROM tv_shows as s JOIN tv_show_genres as g
-ON s.id = g.show_id
-ORDER BY s.title, g.genre_id;
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+INNER JOIN `tv_show_genres` AS g
+ON s.`id` = g.`show_id`
+ORDER BY s.`title`, g.`genre_id`;
