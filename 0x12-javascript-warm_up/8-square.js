07ad373d77d8703ced76ee6426b2d8d89c96d359
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-12-11 15:23:04 +0300

    Fixing task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
-#!/usr/local/bin/node
-const { argv } = require("node:process");
+#!/usr/bin/node
+const { argv } = require('node:process');
 const size = Number(argv[2]);
-if (isNaN(size)) console.log("Missing size");
+if (isNaN(size)) console.log('Missing size');
 else {
   for (let i = 0; i < size; i++) {
-    console.log("X".repeat(size).trimEnd());
+    console.log('X'.repeat(size).trimEnd());
   }
 }
