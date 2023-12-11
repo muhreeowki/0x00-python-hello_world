c31b1b4efa2daed2918cdf8cce2ffff8eca9328f
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-12-11 12:27:08 +0300

    Added task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/local/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        process.stdout.write("X");
+      }
+      console.log("");
+    }
+  }
+}
+module.exports = Rectangle;
