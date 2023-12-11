d3bbb56e161585df5795be6f400041282be1e647
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-12-11 15:42:53 +0300

    Fixing task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 const argv = process.argv;
-if (argv.length == 2) {
+if (argv.length == 3) {
   console.log('Argument found');
-} else if (argv.length > 2) {
+} else if (argv.length > 3) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
