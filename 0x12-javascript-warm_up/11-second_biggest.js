cbe2099ca47fd41e69c4221aec2233ac4cdf4c20
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-12-11 15:30:55 +0300

    Fixing task 8

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,7 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 const argv = process.argv;
 const numbers = argv.map((val) => Number(val)).filter((val) => !isNaN(val));
 numbers.splice(numbers.indexOf(Math.max(...numbers)), 1);
-console.log(Math.max(...numbers));
+const secondBiggest = Math.max(...numbers);
+if (secondBiggest >= 0) console.log(secondBiggest);
+else console.log(0);
