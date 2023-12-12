5984bb1a889e28af7dda837c739fa3491dded198
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-12-12 10:18:59 +0300

    Fixing Styling

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,28 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
-        process.stdout.write("X");
+        process.stdout.write('X');
       }
-      console.log("");
+      console.log('');
     }
   }
 
-  rotate() {
-    let temp = this.width;
+  rotate () {
+    const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
--- 0x13-javascript_objects_scopes_closures/test.js
+++ 0x13-javascript_objects_scopes_closures/test.js
@@ -1,6 +1,6 @@
-#!/usr/local/bin/node
-const logMe = require("./9-logme").logMe;
+#!/usr/bin/node
+const logMe = require('./9-logme').logMe;
 
-logMe("Hello");
-logMe("Best");
-logMe("School");
+logMe('Hello');
+logMe('Best');
+logMe('School');
