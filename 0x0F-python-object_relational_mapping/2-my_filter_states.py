97445e3e2071c0cfd5b26e095726aa78e613a3bf
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-12-15 16:59:40 +0300

    Added task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. """
+"""
+takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where
+name matches the argument.
+"""
 import MySQLdb
 import sys
 
