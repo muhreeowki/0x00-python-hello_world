89e345c555077a3053fb71d64b820403712bb9c5
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-12-15 17:03:23 +0300

    Added task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute(
         """SELECT * FROM states
-        WHERE name={}""".format(
+        WHERE name='{}'""".format(
             state_name
         ),
     )
