fdf3d3b6b33f77adc1657ef75e7de61f86855e57
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-12-22 12:32:25 +0300

    Fixing task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute(
         """
-        SELECT * FROM states WHERE name='{}' ORDER BY id
+        SELECT * FROM states WHERE name LIKE('{}') ORDER BY id
         """.format(
             state_name
         ),
