73f5ba6eb0f1cc86fbac93c320812d7d7aff3739
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-05 13:10:31 +0300

    Added task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a GET request to that URL, and displays the size (in bytes) of the body of the response
-curl -s -X POST -d "$(cat "$2")" $1
+curl -s -H "Content-Type: application/json" -X POST -d "$(cat "$2")" $1
