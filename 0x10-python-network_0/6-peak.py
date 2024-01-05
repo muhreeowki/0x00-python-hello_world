acb4fe5fe9b413e5f6238ea1a8fd9aed937ede00
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-05 12:17:10 +0300

    Added task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module Containing find_peak function"""
+
+
+def find_peak(list_of_integers):
+    """Function that finds the peak in a list"""
+    j = len(list_of_integers) - 1
+    peak = float("-inf")
+    for i in range(j):
+        if i > j:
+            break
+        peak = list_of_integers[j] if peak < list_of_integers[j] else peak
+        peak = list_of_integers[i] if peak < list_of_integers[i] else peak
+        j -= 1
+    return peak
