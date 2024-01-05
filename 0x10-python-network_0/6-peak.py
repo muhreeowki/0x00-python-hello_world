e7ba29bd3996adb7a4d20cd80758f4122e7ba296
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-05 12:43:05 +0300

    Added task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,17 +3,23 @@
 
 
 def find_peak(list_of_integers):
-    """Function that finds the peak in a list"""
-    if len(list_of_integers) == 0:
-        return None
-    peak = float("-inf")
-    for i in range(0, len(list_of_integers), 3):
-        triplet = []
-        triplet.append(list_of_integers[i])
-        if i > 0:
-            triplet.append(list_of_integers[i - 1])
-        if i < len(list_of_integers) - 1:
-            triplet.append(list_of_integers[i + 1])
-        peak = max(triplet)
+    """Finds a peak in list_of_integers"""
 
-    return peak
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    lo = 0
+    hi = len(list_of_integers)
+    mid = int(((hi - lo) // 2) + lo)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if (
+        list_of_integers[mid] >= list_of_integers[mid - 1]
+        and list_of_integers[mid] >= list_of_integers[mid + 1]
+    ):
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
