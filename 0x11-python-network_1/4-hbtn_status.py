3da95d9a85b53ce394a5b78bc58af705aaead54b
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-06 16:03:09 +0300

    Added task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,9 +4,6 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
+    content = response.content.decode()
     print("Body response:")
-    print(
-        "\t- type: {}\n\t- content: {}".format(
-            type(response.content), response.content.decode()
-        )
-    )
+    print("\t- type: {}\n\t- content: {}".format(type(content), content))
