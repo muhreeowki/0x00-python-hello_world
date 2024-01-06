4cdf65ca1df9d937385c5b1b89eed8a6004d9f03
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-06 16:42:03 +0300

    Added task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,8 +5,8 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    try:
-        response = requests.get(url)
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
         print(response.content.decode())
-    except requests.exceptions.HTTPError as error:
-        print("Error code: {}".format(error.code))
