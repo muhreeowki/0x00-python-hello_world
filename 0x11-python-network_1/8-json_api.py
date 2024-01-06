94548ef637d7f6afcbc2631c611e581da954ba2f
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-06 16:54:27 +0300

    Added task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 2 Module"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = "" if len(sys.argv) < 2 else sys.argv[1]
+    response = requests.post(url=url, data={"q": q})
+    try:
+        data = response.json()
+        if "id" in data and "name" in data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
