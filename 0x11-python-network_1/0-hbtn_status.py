29415b853ded6476e11cc08d91c0e5a74b345a5e
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-06 14:23:37 +0300

    Added task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Task 0 Module"""
+import urllib.request
+
+
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    content = response.read()
+    print(
+        "\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
+            type(content), content, content.decode()
+        )
+    )
