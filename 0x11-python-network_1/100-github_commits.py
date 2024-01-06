6a5f1dc9a9e25c1ef0813a29c2f0e66113155363
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-06 18:14:38 +0300

    Added task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,14 +6,14 @@ import sys
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
-    response = requests.get(
-        url=url,
-        headers={"Accept": "application/vnd.github+json"},
-    )
+    response = requests.get(url)
     data = response.json()
-    for i in range(10):
-        print(
-            "{}: {}".format(
-                data[i].get("sha"), data[i].get("commit").get("author").get("name")
+    try:
+        for i in range(10):
+            print(
+                "{}: {}".format(
+                    data[i].get("sha"), data[i].get("commit").get("author").get("name")
+                )
             )
-        )
+    except IndexError:
+        pass
